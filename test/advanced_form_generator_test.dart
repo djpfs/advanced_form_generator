39ec110499dfe17adb40852ee17c607c1470a02d
djpfs <jpfs@icomp.ufam.edu.br>
2022-04-24 14:31:18 -0400

    test

--- test/advanced_form_generator_test.dart
+++ test/advanced_form_generator_test.dart
@@ -1,51 +1,28 @@
-// import 'package:advanced_form_generator/models/FormFieldItem.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:advanced_form_generator/advanced_form_generator.dart';
+import 'package:advanced_form_generator/widgets/FormFieldItem.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:advanced_form_generator/advanced_form_generator.dart';
 
-// void main() {
-//   test('add fields to form', () {
-//     final List<FormFieldItem> formFields = [
-//       FormFieldItem(
-//         title: 'test',
-//         initialValue: 'test',
-//       ),
-//     ];
-//     final form = FormController(formFields);
-//     expect(form.getFields.length, 1);
-//     form.addField(FormFieldItem(title: 'test2'));
-//     expect(form.getFields.length, 2);
-//   });
-//   test('single form to map', () {
-//     final form = FormController([
-//       FormFieldItem(
-//         title: 'test',
-//         initialValue: 'test',
-//       ),
-//       FormFieldItem(
-//         title: 'test2',
-//       ),
-//     ]);
-//     expect(form.toMap(), {'test': 'test', 'test2': null});
-//     expect(form.toMap(emptyToNull: true), {'test': 'test', 'test2': null});
-//     expect(form.toMap(ignoreEmpty: true), {'test': 'test'});
-//   });
-//   test('join forms', () {
-//     final form = FormController([
-//       FormFieldItem(
-//         title: 'test',
-//         initialValue: 'test',
-//       ),
-//     ]);
-//     final form2 = FormController([
-//       FormFieldItem(
-//         title: 'test2',
-//       ),
-//     ]);
-//     final formJoin = FormController.joinForms([form, form2]);
-//     expect(formJoin.getFields.length, 2);
-//     debugPrint(formJoin.toMap(emptyToNull: true));
-//     // expect(formJoin.toMap(emptyToNull: true), {'test': 'test', 'test2': null});
-//     // expect(formJoin.toMap(ignoreEmpty: true), {'test': 'test'});
-//   });
-// }
+void main() {
+  test('create a new form', () {
+    final List<dynamic> formFields = [
+      FormFieldItem(
+        label: 'test',
+        mapKey: 'test',
+        initialValue: 'test',
+      ),
+    ];
+    final form = AdvancedFormGenerator(inputs: formFields);
+    expect(form.getFields.length, 1);
+  });
+  test('test toMap method', () {
+    final List<dynamic> formFields = [
+      FormFieldItem(
+        label: 'test',
+        mapKey: 'test',
+        initialValue: 'test',
+      ),
+    ];
+    final form = AdvancedFormGenerator(inputs: formFields);
+    expect(form.toMap(), {'test': 'test'});
+  });
+}
